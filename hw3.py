1c5ca56e9e4beed9b213d4723acc73731d11763d
Samir <Samirshahub@gmail.com>
2021-08-27 14:48:32 +0300

    IA-140 feat: program that generate folders on a remote computer

---
+++ hw3.py
@@ -0,0 +1,18 @@
+import os, sys
+import paramiko
+
+host, port, user = sys.argv[1], int(sys.argv[2]), sys.argv[3]
+path = os.path.join(sys.argv[4], sys.argv[5])
+count = int(sys.argv[6])
+mode = "0" + str(sys.argv[7])
+key = paramiko.RSAKey.from_private_key_file(sys.argv[8])
+
+client = paramiko.SSHClient()
+client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+client.connect(hostname=host, username=user, port=port, pkey=key)
+
+
+for x in range(1, count + 1):
+    client.exec_command('sudo mkdir '+path + str(x))
+    client.exec_command('sudo chmod ' + mode +" "+ path + str(x))
+client.close()
