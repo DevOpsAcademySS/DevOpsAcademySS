e97bab77b44d9f40e6d6b4d6cffb70fd3473b246
Denis Savchuk <kbob88@gmail.com>
2021-08-31 08:23:30 +0300

    In the script, the eighth task was changed and the explanations were corrected

--- script_regex.sh
+++ script_regex.sh
@@ -2,43 +2,44 @@
 
 
 pattern_surname="[Сс]авчук+[иаіуомхв]{0,1}"
-echo " First task. Input the surname 'Савчук' in Ukrainian letters, you can use different variations or cases."
+echo "----First task. Input the surname 'Савчук' in Ukrainian letters, you can use different variations or cases."
 read surname
 if [[ $surname =~ $pattern_surname ]]; then
-    echo "---The entered value '$surname' matches the pattern"
+    echo "--The entered value '$surname' matches the pattern"
 else
-    echo "---The entered value '$surname' not matches the pattern"
+    echo "--The entered value '$surname' not matches the pattern"
 fi
 
 pattern_name="[Дд](ен|єн)[исаівомхяьчк]{0,1}"
-echo " Second task. Input the name 'Денис' in Ukrainian letters, you can use different variations or cases."
+echo "----Second task. Input the name 'Денис' in Ukrainian letters, you can use different variations or cases."
 read name
 if [[ $name =~ $pattern_name ]]; then
-    echo "---The entered value '$name' matches the pattern"
+    echo "--The entered value '$name' matches the pattern"
 else
-    echo "---The entered value '$name' not matches the pattern"
+    echo "--The entered value '$name' not matches the pattern"
 fi
 
 patern_phone_number="[+]?[ ]?[38]?[( \-]?0[( \-]?37[) \-]?2[)]?[ \-]?[0-9][ \-]?[0-9][ \-]?[0-9][ \-]?[0-9][ \-]?[0-9][0-9]"
-echo "Enter the phone number with code of Chernivtsy 0372, you can use different variations"
+echo "----Third task. Enter the phone number, with a code of Chernivtsy '0372', you can use different variations: spases, '-', '()'."
 read phone_number
 if [[ $phone_number =~ $patern_phone_number ]]; 
 then
-echo "---The entered value '$phone_number' matches the pattern"
+echo "--The entered value '$phone_number' matches the pattern"
 else
-echo "---The entered value '$phone_number' not matches the pattern"
+echo "--The entered value '$phone_number' not matches the pattern"
 fi
 
-echo " Fourth task."
+echo "----Fourth task."
 grep "bash" /etc/passwd > pas.txt && cut -d: -f1 pas.txt
 
-echo " Fifth task."
+echo "----Fifth task."
 grep "daemon" /etc/group
 
-echo " Sixth task."
+echo "----Sixth task."
 grep -v daemon /etc/group
 
-echo " Seventh task."
+echo "----Seventh task."
 ls -R| grep "^README" |wc -l
 
-echo " Eighth task. I have not complied yet."
+echo "----Eighth task."
+find ~ -type f -mmin -600
