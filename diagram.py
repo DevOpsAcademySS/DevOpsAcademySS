7c27b87177b52af4281adb7d83f053917ec6fdb8
day4me <nickydragon1999@gmail.com>
2021-09-02 18:41:15 +0300

    IA-137-mykola-diagrams-as-code Updated python script and uploaded new diagram.

--- diagram.py
+++ diagram.py
@@ -1,4 +1,4 @@
-from diagrams import Cluster, Diagram
+from diagrams import Cluster, Diagram, Edge
 from diagrams.aws.compute import EC2
 from diagrams.aws.database import RDS
 from diagrams.aws.network import ELB
@@ -6,20 +6,24 @@ from diagrams.aws.general import Users
 from diagrams.custom import Custom
 
 with Diagram("Web Service", show=True):
-	Jenkins = Custom(label="\nJenkins",icon_path="./Jenkins_logo.svg")
-	j = Jenkins
 	Web = Custom(label="Geocotizen",icon_path="./web.png")
-	w = Web	 
-	Terraform = Custom(label="Terraform",icon_path="./1686023.png")
-	t = Terraform
-	Ansible = Custom(label="",icon_path="./Ansible_logo.svg")
-	a = Ansible
-	with Cluster("aws"):
+	w = Web	
+	with Cluster("Setup"):
+		Jenkins = Custom(label="\nJenkins",icon_path="./Jenkins_logo.svg")
+		j = Jenkins
+		 
+		Terraform = Custom(label="Terraform",icon_path="./1686023.png")
+		t = Terraform
+		Ansible = Custom(label="",icon_path="./Ansible_logo.svg")
+		a = Ansible
+		j >> Edge() << t
+		j >> a
+	with Cluster("aws", direction="TB"):
 		serv1 = EC2("serv") 
 		serv2 = EC2("serv_database")
-		serv1 - serv2
+		serv1 >> Edge() << serv2
 		aws = [serv1,serv2]
-	Users("DevOps") >> j >> t >> aws
-	t >> j
-	aws >> w << Users("Users")
-	j >> a >> aws	
+	Users("devops")>> j
+	a >> aws
+	t >> aws
+	serv1 >> w >> Edge() << Users("Users")
