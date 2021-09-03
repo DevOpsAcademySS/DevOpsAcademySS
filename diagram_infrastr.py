123bda441f6fb56346a11a35143b6443506449ea
day4me <nickydragon1999@gmail.com>
2021-09-03 16:11:28 +0300

    IA-137-mykola-diagrams-as-code Added Ubuntu and CentOS to diagram_infrastr.py

--- diagram_infrastr.py
+++ diagram_infrastr.py
@@ -3,6 +3,7 @@ from diagrams.aws.compute import EC2
 from diagrams.aws.general import Users
 from diagrams.onprem.iac import Terraform
 from diagrams.onprem.ci import Jenkins
+from diagrams.generic.os import Ubuntu, Centos
 
 with Diagram("Geocitizen Infrastructure", show=True):
 	with Cluster("Setup"):
@@ -12,6 +13,10 @@ with Diagram("Geocitizen Infrastructure", show=True):
 	with Cluster("aws", direction="TB"):
 		serv1 = EC2("serv") 
 		serv2 = EC2("serv_database")
+		Ubnt = Ubuntu("Ubuntu")
+		Cnts = Centos("")
 		aws = [serv1,serv2]
+		serv1 - Cnts
+		serv2 - Ubnt
 	Users("devops")>> Jnkns
 	Trrfrm >> aws
