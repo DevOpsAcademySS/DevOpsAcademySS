89d3f4d0537000a4716b4e4e596d23b598bbd111
Samir <Samirshahub@gmail.com>
2021-09-07 04:24:26 +0300

    IA-140 feat: add output result

--- hw3.py
+++ hw3.py
@@ -1,5 +1,4 @@
-import os, sys
-import paramiko
+import os, sys, paramiko
 
 host, port, user = sys.argv[1], int(sys.argv[2]), sys.argv[3]
 path = os.path.join(sys.argv[4], sys.argv[5])
@@ -11,8 +10,10 @@ client = paramiko.SSHClient()
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 client.connect(hostname=host, username=user, port=port, pkey=key)
 
-
 for x in range(1, count + 1):
     client.exec_command('sudo mkdir '+path + str(x))
     client.exec_command('sudo chmod ' + mode +" "+ path + str(x))
+
+stdin, stdout, stderr = client.exec_command("ls /home")
+print(stdout.readlines())
 client.close()
