6d351ae44dfcc330d4b5a59a04a60d03d15dfcb6
SetMod <voroncka.andrey@gmail.com>
2021-09-22 15:12:53 +0300

    IA-191 fix: fixed sript again 2

--- replcae_ip_inventory.sh
+++ replcae_ip_inventory.sh
@@ -8,7 +8,7 @@ if [[ ! "$WEB_IP" =~ $IP_REGEXP || ! "$DB_IP" =~ $IP_REGEXP ]]; then
 	echo "Wrong format of IP: should be 0.0.0.0 format"
 fi
 
-sed -i "s/{{ web_ip }}/$WEB_IP/" "./inventory.txt"
-sed -i "s/{{ db_ip }}/$DB_IP/" "./inventory.txt"
+sed -i "s/{{ web_ip }}/$WEB_IP/" "inventory.txt"
+sed -i "s/{{ db_ip }}/$DB_IP/" "inventory.txt"
 
 exit 1
