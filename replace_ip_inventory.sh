561fc5c83257ed4ac110dd11a41893c78f279a95
SetMod <voroncka.andrey@gmail.com>
2021-09-22 15:24:06 +0300

    IA-191 fix: added sript back

---
+++ replace_ip_inventory.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+WEB_IP=$1
+DB_IP=$2
+IP_REGEXP="^[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}$"
+
+if [[ "$WEB_IP" =~ $IP_REGEXP && "$DB_IP" =~ $IP_REGEXP ]]; then
+	sed -i "s/{{ web_ip }}/$WEB_IP/" ./inventory.txt
+	sed -i "s/{{ db_ip }}/$DB_IP/" ./inventory.txt
+fi
+
+exit 1
