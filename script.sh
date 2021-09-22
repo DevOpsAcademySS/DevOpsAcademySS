8abdfa014eaf32e7fe178418435b467651771f16
Samir <Samirshahub@gmail.com>
2021-09-22 19:44:07 +0300

    IA-180 fix: delete script.sh

--- script.sh
+++
@@ -1,6 +0,0 @@
-CentosIP=$1
-Ubuntu=$2
-sed -i '/front/s/\/.*:/\/\/'$CentosIP':/' src/main/resources/application.properties
-sed -i '/:postgresql:/s/\/.*:/\/\/'$Ubuntu':/' src/main/resources/application.properties
-sed -i '/export c/s/\/.*:/\/\/'$CentosIP':/' front-end/src/main.js
-sed -i '/p:\/\/r/s/:\/\/r/s:\/\/r/' pom.xml
