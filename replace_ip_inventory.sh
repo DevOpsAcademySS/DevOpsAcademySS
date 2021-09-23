8d861b13e66c009377d42d0007973401605e5a75
SetMod <voroncka.andrey@gmail.com>
2021-09-23 17:17:20 +0300

    IA-191 fix: fixed script again

--- replace_ip_inventory.sh
+++ replace_ip_inventory.sh
@@ -4,9 +4,14 @@ WEB_IP=$1
 DB_IP=$2
 IP_REGEXP="^[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}$"
 
-if [[ "$WEB_IP" =~ $IP_REGEXP && "$DB_IP" =~ $IP_REGEXP ]]; then
-	sed -i "s/{{ web_ip }}/$WEB_IP/" ./inventory.txt
-	sed -i "s/{{ db_ip }}/$DB_IP/" ./inventory.txt
+if  [[ ! "$DB_IP" =~ $IP_REGEXP || ! "$WEB_IP" =~ $IP_REGEXP ]]; # check if IP and PATH valid
+then
+    printf "Set the Servers IPs(0.0.0.0):"
+    printf "\n\n\tExample: $0 yor_WEB_IP yor_DB_IP\n"
+    exit 1
 fi
+
+sed -i "s/{{ web_ip }}/$WEB_IP/" "./inventory.txt"
+sed -i "s/{{ db_ip }}/$DB_IP/" "./inventory.txt"
 
 exit 1
