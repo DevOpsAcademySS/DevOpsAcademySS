d41ad921f47c8c2c58476d52561ff394db35b7a2
Samir <Samirshahub@gmail.com>
2021-09-26 00:49:33 +0300

    IA-179 feat: add security group for each instance

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "Ubuntu" {
   ami = "ami-0d382e80be7ffdae5"
   instance_type = "t2.micro"
   vpc_security_group_ids = [
-    aws_security_group.my_security_group.id]
+    aws_security_group.db_security_group.id]
   tags = {
     Name = "Ubuntu"
   }
@@ -16,21 +16,44 @@ resource "aws_instance" "CentOS" {
   ami = "ami-011996ff98de391d1"
   instance_type = "t2.micro"
   vpc_security_group_ids = [
-    aws_security_group.my_security_group.id]
+    aws_security_group.web_security_group.id]
   tags = {
     Name = "CentOS"
   }
   key_name = "samirus"
 }
 
-resource "aws_security_group" "my_security_group" {
+resource "aws_security_group" "web_security_group" {
+  name = "my_security_group"
+
+  dynamic "ingress" {
+    for_each = [
+      "22",
+      "8080"]
+    content {
+      from_port = ingress.value
+      to_port = ingress.value
+      protocol = "tcp"
+      cidr_blocks = [
+        "0.0.0.0/0"]
+    }
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = [
+      "0.0.0.0/0"]
+  }
+}
+resource "aws_security_group" "db_security_group" {
   name = "my_security_group"
 
   dynamic "ingress" {
     for_each = [
       "5432",
-      "22",
-      "8080"]
+      "22"]
     content {
       from_port = ingress.value
       to_port = ingress.value
