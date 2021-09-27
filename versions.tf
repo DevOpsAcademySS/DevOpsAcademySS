78e8b29fd7945e501f5094300c148f8ef20ad2ea
kdotvozn <64926867+kdotvozn@users.noreply.github.com>
2021-09-27 15:23:23 +0300

    IA-167 feat: configure remote backend

---
+++ versions.tf
@@ -0,0 +1,13 @@
+terraform {
+  backend "s3" {
+    bucket = "devopacademyss-geocitizen-tfstate"
+    encrypt = true
+    key = "AWS-tf_states/terraform.tfstate"
+    region = "eu-north-1"
+  }
+  required_providers {
+    aws = {
+      version = "3.59.0"
+    }
+  }
+}
