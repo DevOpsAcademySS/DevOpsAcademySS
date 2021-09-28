c79ca3ee5b686047ee378fab98b954ae91c65d9e
kdotvozn <kdotvozn@gmail.com>
2021-09-28 13:39:16 +0000

    IA-167 feat: set connection to DynamoDB

--- versions.tf
+++ versions.tf
@@ -1,13 +1,29 @@
 terraform {
   backend "s3" {
     bucket = "devopacademyss-geocitizen-tfstate"
-    encrypt = true
-    key = "AWS-tf_states/jenkins/terraform.tfstate"
+    key = "AWS-tf_states/ubuntu/terraform.tfstate"
+    dynamodb_table = "terraform-state-lock"
     region = "eu-north-1"
+    encrypt = true
   }
   required_providers {
     aws = {
       version = "3.59.0"
     }
+  }
+}
+
+
+# dynamodb table for state file locking
+
+resource "aws_dynamodb_table" "terraform_state_lock" {
+  name           = "terraform-state-lock"
+  hash_key       = "LockID"
+  read_capacity  = 20
+  write_capacity = 20
+
+  attribute {
+    name = "LockID"
+    type = "S"
   }
 }
