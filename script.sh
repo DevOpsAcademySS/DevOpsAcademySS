6413f4361e8c5bb557f5b135140718b5769e00cc
Samir <Samirshahub@gmail.com>
2021-10-04 17:06:46 +0300

    IA-179 fix: add web server ip

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 db_ip="mydbinstance2.cbxkzlsxxhli.eu-west-2.rds.amazonaws.com"
-web_server_ip="localhost"
+web_server_ip="grep -o "[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}" ../terraform/inventory.txt"
 sed -i '/front/s/\/.*:/\/\/'$web_server_ip':/' src/main/resources/application.properties
 sed -i '/:postgresql:/s/\/.*:/\/\/'$db_ip':/' src/main/resources/application.properties
 sed -i '/export c/s/\/.*:/\/\/'$web_server_ip':/' front-end/src/main.js
