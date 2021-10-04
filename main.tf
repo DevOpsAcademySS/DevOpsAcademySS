a74a598211264d38c7bafe73975617fd66de12c8
Samir <Samirshahub@gmail.com>
2021-10-04 16:56:26 +0300

    IA-179 fix: delete db instance

--- main.tf
+++ main.tf
@@ -1,16 +1,6 @@
 provider "aws" {
 }
 
-resource "aws_instance" "Ubuntu" {
-  ami = "ami-0d382e80be7ffdae5"
-  instance_type = "t2.micro"
-  vpc_security_group_ids = [
-    aws_security_group.db_security_group.id]
-  tags = {
-    Name = "Ubuntu"
-  }
-  key_name = "samirus"
-}
 
 resource "aws_instance" "CentOS" {
   ami = "ami-011996ff98de391d1"
@@ -47,40 +37,14 @@ resource "aws_security_group" "web_security_group" {
       "0.0.0.0/0"]
   }
 }
-resource "aws_security_group" "db_security_group" {
-  name = "db_security_group"
-
-  dynamic "ingress" {
-    for_each = [
-      "5432",
-      "22"]
-    content {
-      from_port = ingress.value
-      to_port = ingress.value
-      protocol = "tcp"
-      cidr_blocks = [
-        "0.0.0.0/0"]
-    }
-  }
-
-  egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-    cidr_blocks = [
-      "0.0.0.0/0"]
-  }
-}
 
 
 data "template_file" "dev_hosts" {
   template = file("${path.module}/templates/dev_hosts.cfg")
   depends_on = [
-    aws_instance.Ubuntu,
     aws_instance.CentOS,
   ]
   vars = {
-    api_ubuntu = aws_instance.Ubuntu.public_ip
     api_centos = aws_instance.CentOS.public_ip
   }
 }
--- script.sh
+++ script.sh
@@ -1,7 +1,6 @@
-list_of_ip=$(grep -o "[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}" ../terraform/inventory.txt)
-set -- $list_of_ip
-ss="mydbinstance2.cbxkzlsxxhli.eu-west-2.rds.amazonaws.com"
-sed -i '/front/s/\/.*:/\/\/'$2':/' src/main/resources/application.properties
-sed -i '/:postgresql:/s/\/.*:/\/\/'$ss':/' src/main/resources/application.properties
-sed -i '/export c/s/\/.*:/\/\/'$2':/' front-end/src/main.js
+db_ip="mydbinstance2.cbxkzlsxxhli.eu-west-2.rds.amazonaws.com"
+web_server_ip="localhost"
+sed -i '/front/s/\/.*:/\/\/'$web_server_ip':/' src/main/resources/application.properties
+sed -i '/:postgresql:/s/\/.*:/\/\/'$db_ip':/' src/main/resources/application.properties
+sed -i '/export c/s/\/.*:/\/\/'$web_server_ip':/' front-end/src/main.js
 sed -i '/p:\/\/r/s/:\/\/r/s:\/\/r/' pom.xml
