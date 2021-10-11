2e78744fb265a0832b8a41710e4bcc0e78805382
SetMod <voroncka.andrey@gmail.com>
2021-10-11 13:57:20 +0300

    fix: fixed typo in sh script

--- export_env_vars.sh
+++ export_env_vars.sh
@@ -1,3 +1,3 @@
 export GCP_AUTH_KIND=serviceaccount
-export GCP_SERVICE_ACCOUNT_FILE=~/.ssh/yuor-gcp-cred.json
+export GCP_SERVICE_ACCOUNT_FILE=~/.ssh/your-gcp-cred.json
 export GCP_SCOPES=['https://www.googleapis.com/auth/compute']
