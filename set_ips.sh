7ad3cd703ba4e551a323cda9be01a2902bdbe294
SetMod <voroncka.andrey@gmail.com>
2021-10-14 14:07:23 +0300

    IA-219 feat!,fix!: added new role , scripts, fixes

---
+++ set_ips.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+NEXUS_IP=$1
+WEB_IP=$2
+DOCKER_IP=$3
+IP_REGEXP="^[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}$"
+
+echo "Inputs order: NEXUS_IP WEB_IP DOCKER_IP"
+
+if [[ "$NEXUS_IP" =~ $IP_REGEXP && "$WEB_IP" =~ $IP_REGEXP && "$DOCKER_IP" =~ $IP_REGEXP ]]; then
+	sed -i "s/{{ web_ip }}/$WEB_IP/" ./hosts.txt
+	sed -i "s/{{ nexus_ip }}/$NEXUS_IP/" ./hosts.txt
+	sed -i "s/{{ docker_ip }}/$DOCKER_IP/" ./hosts.txt
+fi
+
+exit 1
---
+++ set_vars.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+NEXUS_IP=$1
+NEXUS_USR=$2
+NEXUS_PASS=$3
+DOCKER_PORT="8085"
+IP_REGEXP="^[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}$"
+
+if [[ "$NEXUS_IP" =~ $IP_REGEXP ]]; then
+	sed -i "s/http:\/\/.*:$DOCKER_PORT/http:\/\/$NEXUS_IP:$DOCKER_PORT/" ./roles/DockerOnUbuntu/files/daemon.json
+
+	sed -i "s/nexus_url:.*/nexus_url: $NEXUS_IP:$DOCKER_PORT/" ./roles/ContainerizeGeo/vars/main.yaml
+	sed -i "s/nexus_usr:.*/nexus_usr: $NEXUS_USR/" ./roles/ContainerizeGeo/vars/main.yaml
+	sed -i "s/nexus_pass:.*/nexus_pass: $NEXUS_PASS/" ./roles/ContainerizeGeo/vars/main.yaml
+
+	sed -i "s/nexus_url:.*/nexus_url: $NEXUS_IP:$DOCKER_PORT/" ./roles/GeocitizenOnUbuntu/vars/main.yaml
+	sed -i "s/nexus_usr:.*/nexus_usr: $NEXUS_USR/" ./roles/GeocitizenOnUbuntu/vars/main.yaml
+	sed -i "s/nexus_pass:.*/nexus_pass: $NEXUS_PASS/" ./roles/GeocitizenOnUbuntu/vars/main.yaml
+fi
+
+exit 1
