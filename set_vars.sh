1f72f2f2b8d007f3728571c39a3c5df49d8007c6
SetMod <voroncka.andrey@gmail.com>
2021-10-14 19:10:21 +0300

    IA-219 fix: fixed script

--- set_vars.sh
+++ set_vars.sh
@@ -8,6 +8,7 @@ IP_REGEXP="^[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}$"
 
 if [[ "$NEXUS_IP" =~ $IP_REGEXP ]]; then
 	sed -i "s/http:\/\/.*:$DOCKER_PORT/http:\/\/$NEXUS_IP:$DOCKER_PORT/" ./roles/DockerOnUbuntu/files/daemon.json
+	sed -i "s/http:\/\/.*:$DOCKER_PORT/http:\/\/$NEXUS_IP:$DOCKER_PORT/" ./roles/ContainerizeGeo/files/daemon.json
 
 	sed -i "s/nexus_url:.*/nexus_url: $NEXUS_IP:$DOCKER_PORT/" ./roles/ContainerizeGeo/vars/main.yaml
 	sed -i "s/nexus_usr:.*/nexus_usr: $NEXUS_USR/" ./roles/ContainerizeGeo/vars/main.yaml
