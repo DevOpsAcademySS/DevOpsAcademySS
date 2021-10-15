81fb364cc81b4193301e60b309cdb7ea2eaed99c
Samir <Samirshahub@gmail.com>
2021-10-15 21:23:10 +0300

    IA-207 feat: load balancing and auto scaling

--- nexus/main.tf
+++ nexus/main.tf
@@ -6,20 +6,43 @@ provider "google" {
   user_project_override = true
 }
 
-resource "google_compute_instance" "ubuntu" {
-  name = "nexus3"
-  machine_type = "e2-medium"
+terraform {
+  backend "gcs" {}
+}
 
-  boot_disk {
-    initialize_params {
-      image = "ubuntu-2004-focal-v20201014"
-    }
+resource "google_compute_instance_template" "my_lamp_instance" {
+  name           = "my-instance-template1"
+  machine_type   = "e2-medium"
+  can_ip_forward = false
+
+  disk {
+    source_image = "ubuntu-2004-focal-v20201014"
   }
 
-   network_interface {
+  network_interface {
     network = "default"
     access_config {
     }
+  }
+
+  connection {
+    host = self.network_interface.0.access_config.0.nat_ip
+    type = "ssh"
+    user = "samuelsharp"
+    private_key = "GCP_SSH.pem"
+    agent = "false"
+    }
+
+  provisioner "remote-exec" {
+//    command = "docker run 2ti2225gz2/geocitizen:5"
+    inline = [
+      "docker run 2ti2225gz2/geocitizen:5",
+      "echo 'sraka'"
+    ]
+  }
+
+  service_account {
+    scopes = ["userinfo-email", "compute-ro", "storage-ro"]
   }
 }
 
@@ -28,14 +51,48 @@ resource "google_compute_firewall" "nexus-rules" {
   network = "default"
   allow {
     protocol = "tcp"
-    ports    = ["8081", "8095"]
+    ports    = ["80", "8080"]
+  }
+  source_ranges = ["209.85.152.0/22", "209.85.204.0/22", "35.191.0.0/16"]
+}
+
+resource "google_compute_target_pool" "my_target_pool" {
+  name = "my-target-pool-3"
+  session_affinity = "NONE"
+}
+
+resource "google_compute_instance_group_manager" "my_group" {
+  name = "my-igm1"
+  zone = var.zone
+
+  version {
+    instance_template  = google_compute_instance_template.my_lamp_instance.id
+    name               = "primary"
+  }
+
+  target_pools       = [google_compute_target_pool.my_target_pool.id]
+  base_instance_name = "geo"
+}
+
+resource "google_compute_autoscaler" "autoscal" {
+  name   = "my-autoscaler"
+  zone   = var.zone
+  target = google_compute_instance_group_manager.my_group.id
+
+  autoscaling_policy {
+    max_replicas    = 4
+    min_replicas    = 2
+    cooldown_period = 60
+
+    cpu_utilization {
+      target = 0.5
+    }
   }
 }
 
-output "ip" {
-  value = google_compute_instance.ubuntu.network_interface.0.access_config.0.nat_ip
-}
-
-terraform {
-  backend "gcs" {}
+resource "google_compute_forwarding_rule" "default" {
+  name                  = "my-rule"
+  target                = google_compute_target_pool.my_target_pool.self_link
+  load_balancing_scheme = "EXTERNAL"
+  ip_protocol           = "TCP"
 }
---
+++ nexus/outputs.tf
@@ -0,0 +1,14 @@
+output "forwarding_rule" {
+  description = "Self link of the forwarding rule (Load Balancer)"
+  value       = google_compute_forwarding_rule.default.self_link
+}
+
+output "load_balancer_ip_address" {
+  description = "IP address of the Load Balancer"
+  value       = google_compute_forwarding_rule.default.ip_address
+}
+
+output "target_pool" {
+  description = "Self link of the target pool"
+  value       = google_compute_target_pool.my_target_pool.self_link
+}
