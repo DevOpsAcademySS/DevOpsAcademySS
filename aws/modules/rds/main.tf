5f5f52197bac675a8b56140351aa6299c8742b8f
SetMod <voroncka.andrey@gmail.com>
2021-10-17 18:00:10 +0300

    fix: fixed rds module

--- aws/modules/rds/main.tf
+++ aws/modules/rds/main.tf
@@ -3,7 +3,7 @@ resource "aws_db_instance" "geo_db" {
   engine            = "postgres"
   engine_version    = "12.5"
   name              = var.db_name
-  instance_type     = var.instance_type
+  instance_class    = var.instance_type
   username          = var.db_user
   password          = var.db_pass
   allocated_storage = 20
