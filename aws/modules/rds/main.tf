604f32d3acec079f7a1b5c123af6f4ed4ccb3590
SetMod <voroncka.andrey@gmail.com>
2021-10-17 19:56:20 +0300

    fix: foxed security group assigin

--- aws/modules/rds/main.tf
+++ aws/modules/rds/main.tf
@@ -1,14 +1,13 @@
 resource "aws_db_instance" "geo_db" {
-  identifier           = var.db_identifire
-  engine               = "postgres"
-  engine_version       = "12.5"
-  name                 = var.db_name
-  instance_class       = var.instance_type
-  username             = var.db_user
-  password             = var.db_pass
-  allocated_storage    = 20
-  security_group_names = [aws_security_group.postgres_sg.name]
-  # parameter_group_name = "default.mysql5.7"
-  skip_final_snapshot = true
-  publicly_accessible = var.pub_access
+  identifier             = var.db_identifire
+  engine                 = "postgres"
+  engine_version         = "12.5"
+  name                   = var.db_name
+  instance_class         = var.instance_type
+  username               = var.db_user
+  password               = var.db_pass
+  allocated_storage      = 20
+  vpc_security_group_ids = [aws_security_group.postgres_sg.id]
+  skip_final_snapshot    = true
+  publicly_accessible    = var.pub_access
 }
