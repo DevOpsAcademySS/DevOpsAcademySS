fb129911248ce10c87ed99423a995b5e3d838794
Samir <Samirshahub@gmail.com>
2021-10-18 03:13:29 +0300

    IA-207 feat: credentials

---
+++ varibles.tf
@@ -0,0 +1,14 @@
+variable "project" {
+  default = "focused-evening-327612"
+}
+variable "credentials" {
+  default = "focused-evening-327612-a15b5b3a8f1e.json"
+}
+
+variable "region" {
+  default = "us-central1"
+}
+
+variable "zone" {
+  default = "us-central1-a"
+}
