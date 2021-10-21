7266dbf5c48bb9ceb010e8789f1069cb1390cec8
balencode <65190424+balencode@users.noreply.github.com>
2021-10-21 19:27:21 +0300

    Update provider.tf

--- tomcat/provider.tf
+++ tomcat/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   shared_credentials_file   = "~/.aws/credentials"
-  profile                   = "terraform"
+  #profile                   = "terraform"
   region                    = var.region
 }
