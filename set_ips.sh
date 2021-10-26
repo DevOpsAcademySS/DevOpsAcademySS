71ac49c8a446e5d8e0b256db4ea0fc5f08fef429
SetMod <voroncka.andrey@gmail.com>
2021-10-26 18:09:11 +0300

    feat: added new role and molecule folders

--- set_ips.sh
+++ set_ips.sh
@@ -3,12 +3,14 @@
 NEXUS_IP=$1
 WEB_IP=$2
 DOCKER_IP=$3
+SONAR_IP=$4
 IP_REGEXP="^[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}$"
 
-echo "Inputs order: NEXUS_IP WEB_IP DOCKER_IP"
+echo "Inputs order: NEXUS_IP WEB_IP DOCKER_IP SONAR_IP"
 
-if [[ "$NEXUS_IP" =~ $IP_REGEXP && "$WEB_IP" =~ $IP_REGEXP && "$DOCKER_IP" =~ $IP_REGEXP ]]; then
+if [[ "$NEXUS_IP" =~ $IP_REGEXP && "$WEB_IP" =~ $IP_REGEXP && "$DOCKER_IP" =~ $IP_REGEXP && "$SONAR_IP" =~ $IP_REGEXP ]]; then
 	sed -i "s/{{ web_ip }}/$WEB_IP/" ./hosts.txt
 	sed -i "s/{{ nexus_ip }}/$NEXUS_IP/" ./hosts.txt
 	sed -i "s/{{ docker_ip }}/$DOCKER_IP/" ./hosts.txt
+	sed -i "s/{{ sonar_ip }}/$SONAR_IP/" ./hosts.txt
 fi
