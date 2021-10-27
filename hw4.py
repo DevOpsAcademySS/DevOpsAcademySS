f5e2d5e01a808697570ede9fc642eb822c1fca76
Denis Savchuk <kbob88@gmail.com>
2021-10-27 20:52:15 +0300

    update-commit

--- hw4.py
+++
@@ -1,8 +0,0 @@
-from sys import argv
-import rpmfile
-def rpmversion(rpmpack_name):
-    with rpmfile.open(rpmpack_name) as rpm_file:
-        print("RELEASE........", rpm_file.headers['release'])
-
-if __name__ == '__main__':
-    rpmversion(argv[1])
