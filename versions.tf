4ab7da1065c920ab52928e3b063fafda3ff643fd
kdotvozn <64926867+kdotvozn@users.noreply.github.com>
2021-11-11 11:31:08 +0200

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@
 terraform {
   backend "s3" {
     bucket = "devopacademyss-geocitizen-tfstate-demo1"
-    key = "AWS-tf_states/ubuntu/terraform.tfstate"
+    key = "AWS/jenkins-ubuntu/terraform.tfstate"
     dynamodb_table = "terraform-state-lock"
     region = "eu-north-1"
     encrypt = true
